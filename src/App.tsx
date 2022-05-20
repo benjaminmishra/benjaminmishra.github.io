fd05c88ddca0d70447f6c92295294d0609ba0ba0
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2022-05-20 11:32:36 +0530

    Inital Commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navigation">
+        <img src={logo} className="logo" alt="" />
+        <div className="navigation-sub">
+          <a href="" className="item">Projects</a>
+          <a href="" className="item">Articles</a>
+          <a href="" className="item">About</a>
+        </div>
+      </div>
     </div>
   );
 }
