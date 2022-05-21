be5b1634be64fbc1b53ccb233b4615a866d94fbe
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2022-05-22 00:06:55 +0530

    Adding nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hero from './components/hero/hero'
 
 function App() {
   return (
     <div className="App">
-      <div className="navigation">
-        <img src={logo} className="logo" alt="" />
-        <div className="navigation-sub">
-          <a href="" className="item">Projects</a>
-          <a href="" className="item">Articles</a>
-          <a href="" className="item">About</a>
-        </div>
-      </div>
+      <Hero/>
     </div>
   );
 }
