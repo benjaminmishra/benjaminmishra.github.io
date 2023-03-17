f6cdcf7cca4e55403a988971bbbb27fc860c835a
Benjamin Mishra <benjaminmishra@gmail.com>
2023-03-17 20:40:23 +0530

    Added code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
-import Hero from './components/hero/hero'
+import Hero from './components/hero/hero';
+import Nav from './components/hero/nav';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <Hero/>
+      <Nav />
+      <Hero />
+      <Footer />
     </div>
   );
 }
