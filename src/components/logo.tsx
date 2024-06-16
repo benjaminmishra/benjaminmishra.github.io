e3d6d9fe8dd868f109cd9913a45918e2462fb404
Benjamin Moloise Mishra <benjaminmishra@gmail.com>
2024-06-16 18:27:33 +0200

    Clicking on logo takes you to home

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 export default function Logo() {
+
+  const handleOnClick = () => {
+    window.location.href = "/";
+  };
+
   return (
     <button>
       <span className="sr-only">Benjamin Logo</span>
       <img
         alt="logo"
         className="h-8 w-auto sm:h-10"
+        onClick={handleOnClick}
         src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
       />
     </button>
