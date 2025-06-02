8809281f9dc66b240ba0416aa1225e6b2b590273
benjaminmishra <benjaminmishra@gmail.com>
2025-06-02 15:06:37 +0000

    Refactor: Update favicon reference in index.html and add tests

--- web/src/components/logo.tsx
+++ web/src/components/logo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../public/favicon.svg";
 
 export default function Logo() {
   const handleOnClick = () => {
@@ -6,26 +7,8 @@ export default function Logo() {
   };
 
   return (
-    <button>
-      <svg width="100" height="30" viewBox="0 0 100 30" xmlns="http://www.w3.org/2000/svg"
-        onClick={handleOnClick}
-      >
-        <style>
-          {`
-          .comment {
-            font-family: 'Courier New', monospace;
-            font-size: 16px;
-            fill: #888; /* Grey color for comments */
-          }
-          .initials {
-            font-family: 'Courier New', monospace;
-            font-size: 20px;
-            fill: #888;
-          }
-          `}
-        </style>
-        <text x="25" y="20" className="initials">[BM]</text>
-      </svg>
+    <button onClick={handleOnClick}>
+      <img src="/favicon.svg" alt="Logo" />
     </button>
   );
 }
