0e34c58c1146e560135a18a7c96e93845c4a4fb1
Benjamin Mishra <benjaminmishra@gmail.com>
2025-06-03 20:18:38 +0200

    fix: correct BlogLandingHeader component name

--- web/src/components/blog-landing-header.tsx
+++ web/src/components/blog-landing-header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function BlogLandigHeader() {
+export default function BlogLandingHeader() {
     return (
         <h2 className="text-slate-900 font-bold font-FigTree text-4xl sm:text-5xl lg:text-6xl tracking-normal text-center dark:text-white">
             My Thoughts
